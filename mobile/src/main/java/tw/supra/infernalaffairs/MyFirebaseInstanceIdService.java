704f32f29a376008d40810ea535b64d72a395764
supra.cn <supra.cn@gmail.com>
2016-09-01 15:21:37 +0800

    集成两个services

---
+++ mobile/src/main/java/tw/supra/infernalaffairs/MyFirebaseInstanceIdService.java
@@ -0,0 +1,6 @@
+package tw.supra.infernalaffairs;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+}
---
+++ mobile/src/main/java/tw/supra/infernalaffairs/MyFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package tw.supra.infernalaffairs;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
